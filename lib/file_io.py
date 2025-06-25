e3fb1a83cd482b34ac1416a1abfb9d496cff2aaf
majidtech25 <abdulshide87@gmail.com>
2025-06-25 20:06:05 +0300

    fileio-labs-done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,43 @@
+import os
+
+
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name)
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+
+    directory = os.path.dirname(file_name)
+    if directory:  
+        os.makedirs(directory, exist_ok=True)
+
+        with open(file_name, 'w') as f:
+             f.write(file_content)
+          
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name)
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+
+    directory = os.path.dirname(file_name)
+    if directory:  
+        os.makedirs(directory, exist_ok=True)
+
+        with open(file_name, 'a') as f:
+             f.write(append_content)
+   
 
 def read_file(file_name):
-    pass
+     file_name = str(file_name)
+     if not file_name.endswith('.txt'):
+        file_name += '.txt'
+
+     directory = os.path.dirname(file_name)
+     if directory:  
+      
+      os.makedirs(directory, exist_ok=True)
+
+     with open (file_name,'r') as f:
+         return f.read()
+  
+  
